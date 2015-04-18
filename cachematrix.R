bc450f276a12e749e04934806e3e7634924b74f3
Shawn Spooner <shawn@sssoftwarecreations.com>
2015-04-18 09:38:13 -0700

    First pass at a memoizable matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Pair of functions to create a special matrix that can memoize its inverse
 
-## Write a short comment describing this function
-
+## Make cache creates a special matrix that allows the result of its inversion to be cached as a property
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        #the matrix has changed, invalidate the cached inverse
+        m <<- NULL      
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Returns the inversion of the matrix x, uses the cached version if it is available
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("using memoized version of inverse")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
